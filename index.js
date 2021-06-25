1f9260af6aa5ced920c6aba34e931b2360a29965
joey <zhaoyi0113@gmail.com>
2021-06-25 17:20:08 +1000

    increase size limit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let app = express();
 const port = 8080;
 
 app.use(express.bodyParser({limit: '50mb'}));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.post('/', cors(), function (req, res) {
   console.log('get a request:', req);
