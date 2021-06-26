c259ce8eb89057aaba873bc0c178c2a37d9be3ca
joey <zhaoyi0113@gmail.com>
2021-06-26 20:37:10 +1000

    add log metrics path

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json({limit: '50mb'}));
 
-app.post('/', cors(), function (req, res) {
-  console.log('get a request:');
+const processRecords = (req, res, type) => {
+	console.log('get a request:');
   const records = req.body.records.map(record => Buffer.from(record.data, 'base64').toString('utf-8'));
   console.log('records:', records);
+	res.set({
+		'X-Amz-Firehose-Protocol-Version': '1.0',
+		'X-Amz-Firehose-Request-Id': req.requestId,
+		'Content-Type': 'application/json',
+	})
   res.json({requestId: req.requestId, timestamp: req.timestamp});
-});
+};
+
+app.post('/logs', cors(), (req, res) => processRecords(req, res, 'logs'));
+
+app.post('/metrics', cors(), (req, res) => processRecords(req, res, 'metrics'));
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`CORS-enabled web server listening on port ${port}`);
