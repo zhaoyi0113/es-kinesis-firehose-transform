4967a8c2ad27169ae08381c2386f03af2b4fa87a
joey <zhaoyi0113@gmail.com>
2021-06-26 20:57:58 +1000

    add bulk request on es

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { default: axios } = require('axios');
+
+const ES_ENDPOINT = 'http://es-entrypoint:9200';
 
 let app = express();
 
 const port = 8080;
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.use(bodyParser.json({ limit: '50mb' }));
 
-const processRecords = (req, res, type) => {
-	console.log('get a request:');
-  const records = req.body.records.map(record => Buffer.from(record.data, 'base64').toString('utf-8'));
+const processRecords = async (req, res, type) => {
+  console.log('get a request:');
+  const records = req.body.records.map((record) => Buffer.from(record.data, 'base64').toString('utf-8'));
   console.log('records:', records);
 
-	const today = new Date();
-	const index = `${type}-${today.getFullYear()}-${today.getMonth()}-${today.getDate()}}`;
+  let data = '';
+  records.forEach((record) => (data += JSON.stringify(record)));
+  const today = new Date();
+  const index = `${type}-${today.getFullYear()}-${today.getMonth()}-${today.getDate()}}`;
 
-
-
-	res.set({
-		'X-Amz-Firehose-Protocol-Version': '1.0',
-		'X-Amz-Firehose-Request-Id': req.requestId,
-		'Content-Type': 'application/json',
-	})
-	console.log('response:', {requestId: req.requestId, timestamp: req.timestamp});
-  res.json({requestId: req.requestId, timestamp: req.timestamp});
+  const url = `${ES_ENDPOINT}/${index}/_bulk`;
+  console.log('send to es:', url, data);
+  axios.post(url, data, {
+    headers: { ContentType: 'application/json' },
+  });
+  res.set({
+    'X-Amz-Firehose-Protocol-Version': '1.0',
+    'X-Amz-Firehose-Request-Id': req.requestId,
+    'Content-Type': 'application/json',
+  });
+  console.log('response:', { requestId: req.requestId, timestamp: req.timestamp });
+  res.json({ requestId: req.requestId, timestamp: req.timestamp });
 };
 
 app.post('/logs', cors(), (req, res) => processRecords(req, res, 'logs'));
