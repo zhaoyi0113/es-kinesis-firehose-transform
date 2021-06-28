5edad964fc4574bbb91742fe6a3666d95bf9d33e
joey <zhaoyi0113@gmail.com>
2021-06-28 12:08:10 +1000

    add timestamp

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const processRecords = async (req, res, type) => {
         try {
           const j = JSON.parse(d);
           records.push({ index: { _index: index } });
-          records.push(j);
+          records.push({...j, '@timestamp': new Date(d.timestamp).toISOString()});
         } catch (err) {}
       });
   });
